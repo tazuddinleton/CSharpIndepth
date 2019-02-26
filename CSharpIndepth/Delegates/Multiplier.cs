58066e1e7238761de37c018ff469261c4931dba0
Taz Uddin <getleton@gmail.com>
2019-02-26 22:08:54 +0600

    Delegates

---
+++ CSharpIndepth/Delegates/Multiplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIndepth.Delegates
+{
+    delegate double Function(double x);
+    public class Multiplier
+    {
+        double factor;
+        public Multiplier(double factor)
+        {
+            this.factor = factor;
+        }
+        public double Multiply(double x)
+        {
+            return x * factor;
+        }
+    }
+}
