29b3db9759c12cb80af5df62ccb4ac31571af220
tazuddinleton <getleton@gmail.com>
2019-06-16 23:16:39 +0600

    Explicit, implicit type conversion

---
+++ CSharpIndepth/Operators/Digit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIndepth
+{
+    public struct Digit
+    {
+        byte value;
+        public Digit(byte val)
+        {
+            if (val > 9)
+            {
+                throw new ArgumentException();
+            }
+            this.value = val;
+        }
+
+        public static explicit operator Digit(byte b)
+        {
+            Digit d = new Digit(b);
+            Console.WriteLine("Explicit Converstion occured");
+            return d;
+        }
+
+        public static implicit operator byte(Digit d)
+        {
+            Console.WriteLine("Implicit Converstion occured");
+            return d.value;
+        }
+    }
+}
---
+++ CSharpIndepth/Operators/TestConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIndepth
+{
+    
+    class TestConversion
+    {
+        static void Main(string[] args)
+        {            
+            try
+            {
+                Digit digit = new Digit(3);
+                byte b = digit; // implicit conversion
+
+                Digit d = (Digit)b; // explicit conversion
+                digit = new Digit(30);
+            }            
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Exception occured.", ex);
+            }
+        }
+    }
+}
