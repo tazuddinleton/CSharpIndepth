0e7dd31f496f2c1c49688ca28934f8414ed6007b
tazuddinleton <getleton@gmail.com>
2019-09-28 14:29:24 +0600

    Generics another example

---
+++ CSharpIndepth/Generics/TestGenerics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSharpIndepth.GenericMethods
+{
+    class TestGenerics
+    {
+        public static void Main(string[] args)
+        {
+            var result = new Result<int>(true, 100);
+            var result2 = new Result<string>(true, "String Data");
+
+            var result3 = new Result<object>(true, new string[] { "Hello", "World" });
+
+                       
+            var printer = new ResultPrinter();
+            printer.Print(result);
+            printer.Print(result2);
+            printer.Print(result3);
+        }
+
+    }
+
+
+
+    public class ResultPrinter
+    {
+        public void Print<T>(Result<T> result)
+        {
+            Console.WriteLine(result.Success);
+            Console.WriteLine(result.Data);
+            Console.ReadLine();
+        }
+    }
+
+    public class Result<T>
+    {
+        public Result(bool s, T d)
+        {
+            Success = s;
+            Data = d;
+        }
+        public bool Success { get; set; }
+        public T Data { get; set; }
+    }
+}
