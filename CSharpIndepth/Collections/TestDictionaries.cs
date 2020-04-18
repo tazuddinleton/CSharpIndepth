6297cc9ada0b3d154131adc4382928015be1e8b6
tazuddinleton <getleton@gmail.com>
2020-04-19 00:54:45 +0600

    grouping things

---
+++ CSharpIndepth/Collections/TestDictionaries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIndepth.Collections
+{
+    public static class TestDictionaries
+    {
+        public static void Main(string[] args)
+        {
+            List<Item> items = GetItems();
+            
+            var group = new ItemGroup<long, Item>(items, item => item.Id);            
+
+            var groupH = group.ToDictionary();
+
+            Console.ReadKey();
+        }
+
+
+
+        private static List<Item> GetItems()
+        {
+            return new List<Item>(){
+            new Item() { Id = 1, Name = "Item1", Amount = 10 },
+            new Item() { Id = 2,  Name = "Item2", Amount = 10 },
+            new Item() { Id = 1, Name = "Item1", Amount = 10 },
+            new Item() { Id = 2, Name = "Item2", Amount = 10 } };
+        }
+    }
+}
---
+++ DSL/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace DSL
+{
+
+    public static class IntExtensions
+    {
+        public static TimeSpan Minutes(this int value)
+        {
+            return TimeSpan.FromMinutes(value);
+        }
+
+        public static bool IsGreaterThan(this int value, int lowerValue)
+        {
+            return value.CompareTo(lowerValue) > 0;
+        }
+    }
+
+    public static class TimeSpanExtensions
+    {
+        public static DateTime FromNow(this TimeSpan value)
+        {
+            return DateTime.Now.Add(value);
+        }
+    }
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var duration = 7.Minutes();
+
+            var when = 15.Minutes().FromNow();
+
+            var tomsAge = 45;
+            var harrysAge = 85;
+
+            if (tomsAge.IsGreaterThan(harrysAge))
+            {
+
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
